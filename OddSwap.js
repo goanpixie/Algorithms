c70e48c96ed6f242471f83461f73ba019cb7d74e
Priyanka <priyanka3007@gmail.com>
2016-10-03 22:30:22 -0400

    All completed

--- OddSwap.js
+++ OddSwap.js
@@ -1,10 +1,51 @@
-function OddSwap(Arr){
-  var temp;
-  for(var i=0;i<Arr.length/2;i+2){
-    temp=Arr[i];
-    Arr[i]=Arr[Arr.length-1-i];
-    Arr[Arr.length-1-i]=temp;
+function swapAltIndices(arr) {
+  var i = 0;
+  var j = arr.length - 1;
+  var isAlt = true;
+  while(i <= j) {
+    if (isAlt === true) {
+      // var temp = arr[i];
+      // arr[i] = arr[j];
+      // arr[j] = temp;
+      [arr[i], arr[j]] = [arr[j], arr[i]]
+      i += 1;
+      j -= 1;
+      isAlt = false;
+    } else {
+     i += 1;
+     j -= 1;
+     isAlt = true
+    }
   }
-  return Arr;
+  return arr;
 }
-console.log(OddSwap([3,1,6,5,4,2]))
+
+
+console.log(swapAltIndices([45,3,1,6,5,4,2,7]))
+
+// Explanation:
+// var arr = [3, 2, 4, 5, 6, 1]
+
+
+// [1, 2, 3]
+
+/*
+ i = 0
+ j = 5
+ 
+ i = 1
+ j = 4
+ isAlt = false
+ 
+ 1 <= 4 SO while loop will proceed
+ 
+ isAlt is false so let's go to else part
+ i = 2
+ j = 3
+ isAlt = true
+ 
+ 2 <= 3
+ isAlt === true
+ 
+ 
+*/
