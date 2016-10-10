615b4d52d401ca928600fc880a5e3e21d8a70870
Priyanka <priyanka3007@gmail.com>
2016-10-10 10:29:39 -0400

    Completed algorithm

--- clock_hand_angles.js
+++ clock_hand_angles.js
@@ -1,11 +1,16 @@
 function clock_hand_angles(secs){
   var time=[];
   var CT=[3600,60,1];
+  if(secs>24*3600){
+    console.log("Give an input less than 24*3600")
+  }
+  else{
   for (var i=0;i<CT.length;i++){
     var quotient= (secs/CT[i]);
     time.push(Math.trunc(quotient));
     var remainder=(secs%CT[i]);
     secs=remainder;
+  }
   }
   var sec_degrees=(time[2]*6);
   console.log(sec_degrees);
@@ -14,10 +19,11 @@ function clock_hand_angles(secs){
   if (time[0]>12){
     var new_hr=time[0]-24
   }
+  else{new_hr=time[0]}
   var hr_degrees=(new_hr*30 + time[1]*0.5 +time[2]*0.0083);
   console.log(hr_degrees);
   
   console.log("Hour hand: ",hr_degrees, "degs","Minute hand: ",min_degrees, "degs","Second hand:",sec_degrees, "degs")
   return(time)
 }
-console.log(clock_hand_angles(119730))
+console.log(clock_hand_angles(36000))
