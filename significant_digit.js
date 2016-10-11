9e9eb7fe0dc58c89a14cf2a76f8fa730827e6f3a
Priyanka <priyanka3007@gmail.com>
2016-10-11 12:12:40 -0400

    Print out the digit greater than 0 to the left most side of the number

---
+++ significant_digit.js
@@ -0,0 +1,20 @@
+function significant_digit(num){
+  var power=1;
+  var truncated_num=Math.trunc(num)
+  var n=truncated_num.toString().split([]).reverse()
+  console.log(n);
+    if (n.length > 0 && n[0]!=0) 
+    {console.log(n[n.length-1])
+      
+    }
+    else {function recall(num){
+      var multipled_num=Math.trunc(num*Math.pow(10,power));
+        if (multipled_num>0){
+          console.log(multipled_num)
+      }
+        power++;
+        recall(multipled_num)
+    }
+    }
+ }
+significant_digit(0.99)
