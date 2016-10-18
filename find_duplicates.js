6333b7df6a600092df1fe9aa038cc5eccb159f39
Priyanka <priyanka3007@gmail.com>
2016-10-17 18:20:53 -0600

    find duplicates

---
+++ find_duplicates.js
@@ -0,0 +1,11 @@
+//fund duplicate number between given array of numbers from 0-10;
+
+function find_duplicates(arr){
+	var Array= (arr.sort(function(a, b){return a-b}))
+
+	var sum=0;
+	for (var i=0;i<Array.length;i++){
+		sum=Array[i]++;
+	}
+	var duplicate_num=55-sum;
+}
