926813429228f1e419d89fa241c3528a2fa2ed81
Priyanka <priyanka3007@gmail.com>
2016-10-17 18:20:23 -0600

    binary search for missing number

---
+++ binary_search_missing_num.js
@@ -0,0 +1,34 @@
+// //find if the given unsorted array contains the given number
+
+// function find_num(arr,num){
+// var start;
+// var end;
+// var Array= (arr.sort(function(a, b){return a-b}));
+// if (Array[Array.length/2]==num){
+// 	return Array.length/2;
+// }
+
+// if(Array[Array.length/2]>num){
+// 	start=0;
+// 	end=(Array.length/2)-1;
+// for(var i=start; i<end ;i++){
+    
+// }
+
+// }
+// else{
+// 	start=Array.length/2;
+// 	end=Array.length;
+
+// }
+
+
+// } 
+
+
+function findNumber (arr, number) {
+	var numberLookup = {};
+	arr.forEach(function () {
+		
+	})
+}
