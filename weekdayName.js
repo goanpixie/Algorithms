10ca3b4309da6c7bdf6eae22463ff6d6cfbdafc6
Priyanka <priyanka3007@gmail.com>
2016-10-31 13:16:18 -0400

    Added the month detector

--- weekdayName.js
+++ weekdayName.js
@@ -117,3 +117,52 @@ function someDays(){
 
 // ----------------------------------------------------------------------------------------------------------------------------------------
 
+// 4.Given a number btw 1-12, gives the month:
+
+function monthName(monthNum){
+  var month;
+  
+  switch(monthNum){
+    case 1:
+      month="January";
+      
+      break;
+    case 2:
+      month="February";
+      break;  
+    case 3:
+     month="March";
+      break;
+    case 4:
+      month="April";
+      break;
+    case 5:
+      month="May";
+      break;
+    case 6:
+     month="June";
+      break;
+    case 7:
+      month="July";
+      break;
+    case 8:
+      month="August";
+      break;
+    case 9:
+      month="September";
+      break;
+    case 10:
+      month="October";
+      break;
+    case 11:
+      month="November";
+      break;
+    case 12:
+      month="Decemeber";
+      break;
+  }
+  console.log("This is ",month)
+}
+
+(monthName(12));
+
