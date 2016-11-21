45efb09f130a976c64f4ca28483f9e3f78375499
Priyanka <priyanka3007@gmail.com>
2016-11-21 12:02:06 -0500

    Algo

--- drop_the_mike.js
+++ drop_the_mike.js
@@ -1,10 +1,20 @@
 function drop_the_mike(str){
   var new_string="";
+  var publish=""
   
   var sentence=str.trim().split(" ");
+     
   for(var i=0;i<sentence.length;i++){
-      new_string+=(sentence[i][0].toUpperCase()+sentence[i].slice(1))+" "
+      new_string=(sentence[i][0].toUpperCase()+sentence[i].slice(1))+" ";
+      var compared_word=(new_string.trim())
+      
+      if(compared_word =="Mike"){
+        return "stunned silence"
+      }
+  publish=publish+(new_string)
   }
-  return new_string.trim();
+  
+  return publish
+  
 }
-console.log(drop_the_mike(" lets do this! "))
+console.log(drop_the_mike(" lets do this Mike ! "))
