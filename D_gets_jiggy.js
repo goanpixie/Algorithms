f9442e97a790579a7adb209a033789f037a0d4c9
Priyanka <priyanka3007@gmail.com>
2016-11-21 11:24:02 -0500

    Algo

--- D_gets_jiggy.js
+++ D_gets_jiggy.js
@@ -1,7 +1,3 @@
-function D_gets_jiggy(str){
-  
-  var capitalize_cut =str.charAt(1).toUpperCase()+str.slice(2);
-  
-  return capitalize_cut +" to the "+str[0]+ "!";
+function chk_alphabetical_order(str){
+	
 }
-console.log(D_gets_jiggy("Dylan"))
