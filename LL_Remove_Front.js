b391b8c866a17be29573350c9f301676f48fbbca
Priyanka <priyanka3007@gmail.com>
2016-11-23 09:20:11 -0500

    LL REmove Front case 2

--- LL_Remove_Front.js
+++ LL_Remove_Front.js
@@ -1,3 +1,26 @@
+// Case1:
+
+function Remove_Front(starting_pointer){
+  var head=starting_pointer;
+  if(head.next){
+  var save=head.next;
+  head=save;
+  return head;
+}
+return "null_value";
+}
+
+var starting_pointer={
+  value:4,
+  next:null
+ 
+}
+
+var linkedlist=Remove_Front(starting_pointer);
+console.log(linkedlist)
+
+// Case2:
+
 function Remove_Front(starting_pointer){
   var head=starting_pointer;
   if(head.next){
