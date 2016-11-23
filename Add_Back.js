faf8c33535cb8c5ce17da09c61a747534d43ebf5
Priyanka <priyanka3007@gmail.com>
2016-11-23 12:01:08 -0500

    Algo

---
+++ Add_Back.js
@@ -0,0 +1,36 @@
+function Add_Back(starting_pointer){
+  var head=starting_pointer;
+  var newNode={
+    value:8,
+    next:null
+  }
+  var runner=head.next;
+  while(runner){
+    console.log(runner.value)
+    if(runner.next==null){
+      runner.next=newNode;
+      newNode.next=null
+    }
+      runner=runner.next;
+  }
+  return head
+  
+  
+}
+
+var starting_pointer={
+  value:4,
+  next:{
+    value:5,
+    next:{
+      value:6,
+      next:{
+        value:7,
+        next:null
+      }
+    }
+  }
+}
+
+var linkedlist=Add_Back(starting_pointer);
+console.log(linkedlist);
