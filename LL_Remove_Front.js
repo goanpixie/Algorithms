6e2c16747bf2774723bfdd258bfb625401df7923
Priyanka <priyanka3007@gmail.com>
2016-11-23 09:16:52 -0500

    LL Remove Front

---
+++ LL_Remove_Front.js
@@ -0,0 +1,18 @@
+function Remove_Front(starting_pointer){
+  var head=starting_pointer;
+  if(head.next){
+  var save=head.next;
+  head=save;
+  return head;
+}
+return "null_value";
+}
+
+var starting_pointer={
+  value:4,
+  next:null
+ 
+}
+
+var linkedlist=Remove_Front(starting_pointer);
+console.log(linkedlist)
