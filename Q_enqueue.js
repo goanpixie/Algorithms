6244de4835851aefca565e819dd6635350e56aef
Priyanka <priyanka3007@gmail.com>
2016-11-27 15:08:28 -0500

    Algo added

--- Q_enqueue.js
+++ Q_enqueue.js
@@ -14,9 +14,18 @@ function peek(){
   console.log(queue[0]);
 }
 
+function contains(val){
+  for(var i=0;i<queue.length;i++){
+    if(queue[i]==val){
+      return "Value found"
+    }
+  }
+  return "Given value is not present"
+}
+
 console.log(enqueue(2))
 console.log(enqueue(3))
 console.log(enqueue(4))
 console.log(dequeue())
 console.log(peek())
-
+console.log(contains(4))
