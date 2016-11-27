c26562a2f980c0e703d61bd498d7bfe40d799d03
Priyanka <priyanka3007@gmail.com>
2016-11-27 14:58:43 -0500

    Added changes

--- Q_enqueue.js
+++ Q_enqueue.js
@@ -1,10 +1,17 @@
+
 var queue = [];
 function enqueue(val){
   queue.push(val);
   return(queue)
 }
 
+function dequeue(){
+  queue.shift();
+  return (queue)
+}
+
 console.log(enqueue(4))
 console.log(enqueue(5))
 console.log(enqueue(5))
+console.log(dequeue())
 
