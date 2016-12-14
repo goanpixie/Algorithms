0bc71e3f79ea1a183c0861c2dadc218a1f6bd91e
Priyanka <priyanka3007@gmail.com>
2016-12-14 15:48:53 -0500

    Algorithms

---
+++ interleaved_str.js
@@ -0,0 +1,11 @@
+function interleaved_str(str1,str2,str3){
+  for(var i =0; i<str1.length;i++){
+    if(str3[i+i]!==str1[i] || str3[i+1+i]!==str2[i]){
+      return false;
+    }
+  
+  }
+  return true;
+ 
+}
+console.log(interleaved_str("dne","ail","daniel"))
