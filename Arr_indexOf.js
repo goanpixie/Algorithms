ce40b1720f659debc64bd8650cb52a92adaf9136
Priyanka <priyanka3007@gmail.com>
2017-04-02 18:27:01 -0700

    indexOf

---
+++ Arr_indexOf.js
@@ -0,0 +1,11 @@
+var arr = ["sita","babita", "geeta"]
+// var babitaExits =(arr.indexOf("babita"))>-1
+
+// if(!babitaExits){
+//   arr.push("babita")
+// }
+// console.log(arr)
+
+
+var babitaExits =(arr.indexOf("babita",1))>-1        
+ console.log(babitaExits)    
