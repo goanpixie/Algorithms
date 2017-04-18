02fb771898cdeff848755bb043ba763c50cdf27b
Priyanka <priyanka3007@gmail.com>
2017-04-18 13:40:24 -0700

    Handles any number of arguments

--- find_median.js
+++ find_median.js
@@ -1,3 +1,5 @@
+// #Handles two arguments:
+
 function median(arr1,arr2){
   var newArr=arr1.concat(arr2).sort();
   console.log(newArr)
@@ -12,3 +14,26 @@ function median(arr1,arr2){
 } 
 
 console.log(median([1,5,9],[1,2,3,4,5,6]))
+
+
+// #Handles arbitary number of arguments:
+
+var newArr=[];
+function median(arr1,arr2,arr3,arr4){
+  for(var i=0; i<arguments.length;i++){
+    
+  newArr=newArr.concat(arguments[i]).sort();
+  console.log(newArr)
+    
+  }
+   if(newArr.sort().length%2==0){
+     return (newArr[((newArr.length/2)+1)]+newArr[((newArr.length/2)-1)])/2
+   }
+   else{
+     return (newArr[Math.floor(newArr.length/2)])
+   }
+   
+  
+} 
+
+console.log(median([3,2,1],[4,5,6],[7,8,9]))
