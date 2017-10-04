c0124db7021c9a48e12299f56f375a79c240250f
Priyanka <priyanka3007@gmail.com>
2017-10-04 11:55:13 -0700

    Added another way to solve the problem

--- Palindrome.js
+++ Palindrome.js
@@ -52,4 +52,15 @@ var isPalindrome = function(x) {
 };
 
 
-//
+//Matter time complexity:
+var isPalindrome = function(x) {
+    if(x<0 || (x%10 == 0 && x != 0)){
+        return false;
+    }
+    var num = 0;
+    while(x > num){
+        num = num*10 + x%10;
+        x = (x-x%10)/10;
+    }
+    return x == num || x == (num-num%10)/10;
+};
