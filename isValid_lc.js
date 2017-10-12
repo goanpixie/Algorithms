10fc6339514aabc6e29d7b70ea2d80eb724c89cc
Priyanka <priyanka3007@gmail.com>
2017-10-11 17:40:26 -0700

    need to better the speed

---
+++ isValid_lc.js
@@ -0,0 +1,37 @@
+//
+//Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+//The brackets must close in the correct order, "()" and "()[]{}" are all valid but "(]" and "([)]" are not.
+
+function isValid(s){
+	if (s.charAt(0) === "}" ||s.charAt(0)=== ")" ||s.charAt(0)=== "]" ){
+    return false;
+  }
+  var lc = s.slice(-1);
+	if (lc=== "{" || lc ==="(" ||lc === "[" ){
+    return false;
+  } 
+  else{
+    var stack =[];
+    var opening_brackets = "({[";
+    
+     for(var i=0;i<s.length;i++){
+     		if(opening_brackets.indexOf(s[i]) >-1 ){
+          stack.push(s[i]);
+        	}
+        else{
+        console.log(s[i])
+        	var combo = (stack.pop() + s[i]);
+        	if(combo === "()" || combo ==="{}"|| combo ==="[]"){     
+          }
+          else{
+          return false;
+          }
+        }  
+   	 }
+ 	}
+  if(stack.length!==0){
+  return false;
+  }
+ return true;
+}
