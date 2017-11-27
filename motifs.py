24e7bfc1bbf88ac682d0f92ec2dc193ca9678e54
Priyanka <priyanka3007@gmail.com>
2017-11-27 13:19:20 -0800

    Motifs.py

---
+++ motifs.py
@@ -0,0 +1,16 @@
+# Input:  A set of kmers Motifs
+# Output: Count(Motifs)
+def Count(Motifs):
+    count = {}
+    # initializing the count dictionary
+    k = len(Motifs[0])
+    for symbol in "ACGT":
+        count[symbol] = []
+        for j in range(k):
+             count[symbol].append(0)
+    t = len(Motifs)
+    for i in range(t):
+        for j in range(k):
+            symbol = Motifs[i][j]
+            count[symbol][j] += 1
+    return count
