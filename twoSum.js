62534ef8c67f3d53a68406dd801e28c8f2390ac1
Priyanka R Shah <priyankarshah@Priyankas-MacBook-Pro.local>
2020-07-30 22:49:12 -0700

    My init commit for 2020

---
+++ twoSum.js
@@ -0,0 +1,21 @@
+//Given an array if the sum of any two numbers is 0 return true else false.
+
+function twoSum(arr){
+    //sort the array in ascending order
+    let sortedArr = arr.sort((a,b)=>(b-a))
+    let startWindow =0;
+    let endWindow = arr.length-1;
+    while(startWindow<endWindow){
+        let sum = sortedArr[startWindow]+sortedArr[endWindow];
+        if(sum ===0){
+            return true;
+        }else if(sum>0){
+            endWindow--
+        }else{
+            startWindow++
+        }
+    }
+    return true;
+}
+
+console.log(twoSum([-4,3,5,7,4]))
